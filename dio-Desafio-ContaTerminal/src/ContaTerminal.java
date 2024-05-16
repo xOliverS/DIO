a8fe72ad328a0e977681dc76a4b6892700fbf0b2
xOliverS <samuelcambio@gmail.com>
2024-05-15 21:47:51 -0300

    Incluindo cifrão

--- dio-Desafio-ContaTerminal/src/ContaTerminal.java
+++ dio-Desafio-ContaTerminal/src/ContaTerminal.java
@@ -25,7 +25,7 @@ public class ContaTerminal {
         saldo = scan.nextDouble();
 
 
-        System.out.println("Olá " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta " +numeroConta+ " e seu saldo " +saldo+ " já está disponível para saque");
+        System.out.println("Olá " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta " +numeroConta+ " e seu saldo R$ " +saldo+", já está disponível para saque.");
 
         scan.close();
 
